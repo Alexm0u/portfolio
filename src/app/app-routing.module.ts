12b5c137bdedda679ba4cd91a196b0e7b1215314
alexm0u <alexm0u.13@gmail.com>
2023-10-23 09:37:06 +0200

    Enrutado y colocación del navbar básico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SobreMiPageComponent } from './pages/sobre-mi-page/sobre-mi-page.component';
+import { ProyectosPageComponent } from './pages/proyectos-page/proyectos-page.component';
+import { ContactoPageComponent } from './pages/contacto-page/contacto-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'sobre-mi', component: SobreMiPageComponent},
+  {path: 'proyectos', component: ProyectosPageComponent},
+  {path: 'contacto', component: ContactoPageComponent},
+  {path: '**', component: HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProyectosPageComponent } from './pages/proyectos-page/proyectos-page.component';
 import { SobreMiPageComponent } from './pages/sobre-mi-page/sobre-mi-page.component';
+import { ContactoPageComponent } from './pages/contacto-page/contacto-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     ProyectosPageComponent,
-    SobreMiPageComponent
+    SobreMiPageComponent,
+    ContactoPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
