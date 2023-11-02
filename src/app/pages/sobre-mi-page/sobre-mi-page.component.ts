45c551634ce2cd700b278a96f53f7ab93c535490
alexm0u <alexm0u.13@gmail.com>
2023-11-02 13:27:52 +0100

    Primera versión de la sección SObre mí

--- src/app/pages/sobre-mi-page/sobre-mi-page.component.ts
+++ src/app/pages/sobre-mi-page/sobre-mi-page.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class SobreMiPageComponent {
 
+  sobreMiListState: boolean = false;
+  contactoListState: boolean = false;
+  softListState: boolean = false;
+  tecListState: boolean = false;
+
+  toggleList(listName: string) {
+    switch (listName) {
+      case 'sobreMi':
+        this.sobreMiListState = !this.sobreMiListState;
+        break;
+      case 'contacto':
+        this.contactoListState = !this.contactoListState;
+        break;
+      case 'soft':
+        this.softListState = !this.softListState;
+        break;
+      case 'tec':
+        this.tecListState = !this.tecListState;
+        break;
+      default:
+        break;
+    }
+  }
 }
